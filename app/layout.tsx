9d1a80dcf832911e0c20a7cb3679c1319cea2c1d
Martin Maiaru <tincho_91_15@hotmail.com>
2023-09-04 09:57:36 -0300

    DEMO 2 - 7

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css'
 const font = Urbanist({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Giardelli-Gonzales',
+  title: 'Giardelli-Gonzalez',
   description: 'Búsqueda de empleos.',
 }
 
