3c217deb27159e32b089923129ca298863327550
Martin Maiaru <tincho_91_15@hotmail.com>
2023-12-16 00:17:19 +0100

    DEMO FINAL 16/12 - 2

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="ml-4 flex md:hidden lg:flex lg:ml-0 gap-x-2">
             <Link href="/">
-              <img src="/brand.png" alt="CONSULTORA" className="h-9 sm:h-10 w-full" />
+              <img src="/brand.png" alt="CONSULTORA" className="h-9 sm:h-10 w-full max-w-[80%]" />
             </Link>
           </div>
           <div className="ml-4 lg:ml-0 gap-x-2 hidden md:flex lg:hidden">
