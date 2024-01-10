9489d50d38c1d6bb934c9b2e71b63652981dff67
Martin Maiaru <tincho_91_15@hotmail.com>
2024-01-10 14:49:19 +0100

    DEMO FINAL 10/1

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ['/:path*' ]
-});
+export default authMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
