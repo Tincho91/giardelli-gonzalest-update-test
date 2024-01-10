f3c8cab51ce956eca8f011a7f1bb05dbc9e3b0f3
Martin Maiaru <tincho_91_15@hotmail.com>
2024-01-10 14:44:39 +0100

    DEMO FINAL 10/1

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ['/:path*', '/cv/:path*', '/positions/:path*' ]
+  publicRoutes: ['/:path*' ]
 });
 
 export const config = {
