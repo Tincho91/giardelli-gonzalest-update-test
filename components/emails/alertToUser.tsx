f10621cf4ccb3ecd41f04f5a4f98b970fe586f0b
Martin Maiaru <tincho_91_15@hotmail.com>
2024-02-27 09:29:42 +0100

    FINAL 27/2

--- components/emails/alertToUser.tsx
+++ components/emails/alertToUser.tsx
@@ -12,7 +12,7 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   application,
 }) => (
   <div className="bg-slate-500">
-    <h3 className="py-5">¡Hola {user.name}! Recibimos tu aplicacioón para el puesto "{application.name}".</h3>
+    <h3 className="py-5">¡Hola {user.name}! Recibimos tu aplicación para el puesto "{application.name}".</h3>
     <h3 className="pb-5">Desde nuestro equipo, vamos a estar revisando tu CV, y si tu perfíl se ajusta a los requisitos de la búsqueda, nos pondremos en contacto para coordinar una entrevista. ¡Agradecemos tu interés en participar en este proceso de selección!</h3>
     <p className="pb-1">Saludos,</p>
     <p>Giardelli Gonzalez.</p>
--- components/emails/userPositionApplication.tsx
+++ components/emails/userPositionApplication.tsx
@@ -14,8 +14,8 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   application,
 }) => (
   <div className="bg-slate-500">
-    <h1 className="text-4xl font-bold">Nuevo Aplicante para {application.name}</h1>
-    <p className="py-5">El usuario {user.name} ha aplicado al puesto de trabajo.</p>
+    <h3 className="py-5">Nuevo Aplicante para {application.name}</h3>
+    <p className="pb-5">El usuario {user.name} ha aplicado al puesto de trabajo.</p>
     <p className="pb-5">CV: {user.cv}</p>
     <p>Email: {user.email}</p>
   </div>
