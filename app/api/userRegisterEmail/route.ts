0b931f7e7a7a4179768b00adef093bc22fb4516b
Martin Maiaru <tincho_91_15@hotmail.com>
2024-02-28 00:06:06 +0100

    FINAL 27/2

--- app/api/userRegisterEmail/route.ts
+++ app/api/userRegisterEmail/route.ts
@@ -23,21 +23,17 @@ export async function POST(request: any) {
                 ¡Hola ${formData.user.name}! Gracias por sumarte a nuestra web! Ya sos parte de nuestra base de datos.
             </h4>
             <br>
-            <br>
             <h4>
                 Si tu perfil coincide con alguna de nuestras búsquedas activas, nos pondremos en contacto para coordinar una entrevista.
                 ¡Agradecemos tu interés en formar parte de nuestro equipo!
             </h4>
             <br>
-            <br>
             <h4>
                 Saludos,
             </h4>
-            <br>
             <h4>
                 Giardelli & Gonzalez
             </h4>
-            <br>
             <img src="https://giardelli-gonzalez-web.vercel.app/brand.png" alt="Logo" style="width: 250px; height: auto;">
     
             `
